7b07a8665e9de83df28ac6eea606d3f07030119a
Wesley-waka <wesleywaka77@gmail.com>
2024-02-22 04:24:35 +0300

    displaying data in accordion

--- components/accordion.jsx
+++ components/accordion.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 const Accordion = ({ title, answer }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
+  
 
   return (
     <div className="py-2">
