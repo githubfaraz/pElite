11735ac65a72938d08f7fab811f16b32209319cd
Wesley-waka <wesleywaka77@gmail.com>
2024-02-26 16:05:35 +0300

    Form Contact US submission to Form Spree

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,12 +1,17 @@
+'use client'
 import Footer from '@/components/Footer'
 import MainNavbar from '@/components/MainNavbar'
 import Navb from '@/components/Navb'
 import Navbar from '@/components/Navbar'
 import Image from 'next/image'
 import React from 'react'
-
+import { useForm, ValidationError } from '@formspree/react';
 // Note:remember to remove the navbar
-const page = () => {
+const Page = () => {
+    const [state, handleSubmit] = useForm("mayrjoke");
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
   return (
     <div>
         
@@ -30,30 +35,62 @@ const page = () => {
                 </div>
             </div>
             <div className=' w-full'>
-                <form >
+            <form
+            method="POST"
+            action="https://formspree.io/f/mayrjoke"
+            >
                     <div className='flex sm:flex-row sm:space-x-2 flex-col space-y-4 sm:space-y-0'>
                         <div className='flex flex-col'>
-                            <label className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>First Name</label>
-                            <input className='p-2 w-72 rounded-lg border-2 border-gray-400' type="email" style={{borderWidth: '0.75px'}}/>
+                            <label htmlFor="first_name" className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>First Name</label>
+                            <input className='p-2 w-72 rounded-lg border-2 border-gray-400' type="text"
+                                    name="first_name"
+                                    id="first_name"
+                                    placeholder="First Name"
+                                    aria-required="true"
+                                    required
+                                    style={{borderWidth: '0.75px'}}
+                            />
                         </div>
                         <div className='flex flex-col'>
-                            <label className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Second Name</label>
-                            <input className='p-2 w-72 rounded-lg border-2 border-gray-400' style={{borderWidth: '0.75px'}} type="email"/>
+                            <label htmlFor="second_name"  className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Second Name</label>
+                            <input className='p-2 w-72 rounded-lg border-2 border-gray-400'
+                                name="second_name"
+                                id="second_name"
+                                placeholder="Second Name"
+                                aria-required="true"
+                                required
+                            style={{borderWidth: '0.75px'}} />
                         </div>
                     </div>
                     <div className='flex flex-col sm:space-y-2 mt-4 w-full'>
-                        <label className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Email Address</label>
-                        <input className='p-2 w-full rounded-lg border-2 border-gray-400' style={{borderWidth: '0.75px'}} type="email"/>
+                        <label htmlFor="email_address"  className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Email Address</label>
+                        <input className='p-2 w-full rounded-lg border-2 border-gray-400'
+                            name="email"
+                            id="email"
+                            placeholder="Email"
+                            aria-required="true"
+                            required
+                            style={{borderWidth: '0.75px'}} 
+                            type="email"/>
                     </div>
                     <div className='flex flex-col space-y-2 mt-4 ' >
-                        <label className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Phone Number</label>
-                        <input className='p-2 w-full rounded-lg border-2 border-gray-400' style={{borderWidth: '0.75px'}} />
+                        <label htmlFor="phone_number" className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Phone Number</label>
+                        <input className='p-2 w-full rounded-lg border-2 border-gray-400' 
+                            name="phone_number"
+                            id="phone_number"
+                            placeholder="Phone Number"
+                            aria-required="true"
+                            required
+                            style={{borderWidth: '0.75px'}} />
+                       
                     </div>
                     <div className='flex flex-col space-y-2 mt-4'>
-                        <label className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Message</label>
-                        <textarea name="message" id="message" cols={30} rows={5} style={{borderWidth: '0.75px'}} className='p-2 w-full rounded-lg border-2 border-gray-400 '/>
+                        <label htmlFor="message" className='ml-2 font-medium text-lg' style={{fontFamily: "playfair_display"}}>Message</label>
+                        <textarea name="message" id="message" placeholder="Message"
+                            aria-required="true"
+                            required cols={30} rows={5} style={{borderWidth: '0.75px'}} className='p-2 w-full rounded-lg border-2 border-gray-400 '/>
                     </div>
-                    <button className='mt-4 text-center bg-burgundy text-white py-3 px-4 w-full rounded-2xl border-4 border-slate-200 text-lg font-semibold hover:bg-white hover:text-burgundy hover:border-burgundy hover:transition-all' style={{fontFamily: "poppins",borderWidth: '2px'}}>Send Message</button>
+                    <button type="submit" aria-label="Send Message" className='mt-4 text-center bg-burgundy text-white py-3 px-4 w-full rounded-2xl border-4 border-slate-200 text-lg font-semibold hover:bg-white hover:text-burgundy hover:border-burgundy hover:transition-all' style={{fontFamily: "poppins",borderWidth: '2px'}}>Send Message</button>
                 </form>
             </div>
             
@@ -66,4 +103,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
