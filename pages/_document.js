72cb48b5724112ad24d7240892c9a7dfe5cd54f6
Wesley-waka <wesleywaka77@gmail.com>
2024-03-05 16:02:48 +0300

    Error rectification

--- pages/_document.js
+++ pages/_document.js
@@ -5,30 +5,7 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>,
-        ],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
 
   render() {
     return (
