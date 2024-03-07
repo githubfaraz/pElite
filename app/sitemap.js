bb7efb2e08b74e9e079214140c029c20f3cf36e3
Wesley-waka <wesleywaka77@gmail.com>
2024-03-07 15:41:58 +0300

    Blogs for SEO

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,8 +4,8 @@ export default async function sitemap(){
     const response = await fetch("https://pets-elite-b.fly.dev/api/blogs")
     const blogs = await response.json();
     
-    const blogEntries = blogs.map(({title})=>({
-        url: `https://pets-elite-b.fly.dev/api/blogs/${title}`,
+    const blogEntries = blogs.map(({_id})=>({
+        url: `https://pets-elite-b.fly.dev/api/blogs/${_id}`,
         // lastModified: new Date(post.updatedAt)
         // changeFrequency:,
         // priority:
