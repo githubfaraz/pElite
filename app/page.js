755b73709adc5776ad5b4a3a6532618bd16ed69e
Wesley-waka <wesleywaka77@gmail.com>
2024-03-25 16:55:14 +0300

    Enhance metadata

--- app/page.js
+++ app/page.js
@@ -12,14 +12,12 @@ import useScript from "@/hooks/useScript";
 import Image from "next/image";
 import Link from "next/link";
 import Head from 'next/head';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/swiper-bundle.css';
 
-export const metadata = {
+export const metadata= async() => {
+  return{  
   metadataBase: new URL("https://www.elitetreatforpets.com"),
   title: "Elite Treat For Pets",
-  description: "Shop the best selection of all-natural bully sticks at Elite Treat For Pets. Keep your dog healthy and happy with our high-quality, digestible, and long-lasting chews.",
   appleMobileWebAppTitle: 'Elite Treat For Pets',
   authors: [
     {
@@ -31,24 +29,6 @@ export const metadata = {
     card: "summary_large_image",
     title: "Elite Treat For Pets",
     images: "https://www.elitetreatforpets.com/logoP.png"
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  alternates: {
-    canonical: `https://www.elitetreatforpets.com/`,
-    languages: {
-      "en-US": "/",
-    },
   },
   openGraph: {
     type: "website",
@@ -64,23 +44,13 @@ export const metadata = {
     ],
   },
   assets: "https://www.elitetreatforpets.com/logoP.png",
-  keywords: [
-    "Elite Treat For Pets","Bully Stick","Treats,Pets","BullyStick","Pet Treat","Bully sticks","Dog chews","Natural dog treats","Elite bully sticks","Premium dog chews","Best bully sticks for puppies","Long-lasting dog chews natural","Organic bully sticks for dogs","High-quality beef pizzle sticks"
-  ],
+}
 }
 
 function Home() {
 
   return (
     <>
-    <Head>
-        <title>
-        
-        Elite Treat For Pets
-        
-        </title>
-      <link rel="icon" type="image/svg+xml" href="/Plogo.png"></link>
-    </Head>
     <div>
 
       {/* Hero section */}
