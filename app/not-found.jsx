35ac5d4fe63d6439d1f18ba0de1dd7301501cc20
Wesley-waka <wesleywaka77@gmail.com>
2024-04-09 14:22:21 +0300

    Add not found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const notFound = () => {
   return (
-    <div>
-      NOT FOUND
+    <div className='mx-auto py flex flex-col space-y-4'>
+      <Image alt='nfound' className='w-96 h-80 mx-auto' src='noFound.svg' height={90} width={100}/>
+      <Link href='/' className='px-20 py-4 rounded-md mx-auto w-fit bg-orange-400 text-white'>Return Home</Link>
     </div>
   )
 }
